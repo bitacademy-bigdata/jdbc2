cd6506a81ca89cb08e5fcdae7775b677db8d82ca
BIT <BIT@DESKTOP-S4KDBK1>
2019-12-18 13:48:02 +0900

    jdbc insert test

--- src/test/InsertTest.java
+++ src/test/InsertTest.java
@@ -2,17 +2,22 @@ package test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class SelectTest {
-
+public class InsertTest {
 	public static void main(String[] args) {
+		Boolean result = insert("도", "우넛", "donut@gmail.com");
+		System.out.println(result);
+		
+		result = insert("또", "치", "ddochi@gmail.com");
+		System.out.println(result);
+	}	
+	
+	public static Boolean insert(String firstName, String lastName, String email) {
+		Boolean result = false;
 		Connection conn = null;
 		Statement stmt = null;
-		ResultSet rs = null;
-		
 		try {
 			//1. JDBC Driver(Mysql) 로딩
 			Class.forName("com.mysql.jdbc.Driver");
@@ -20,29 +25,18 @@ public class SelectTest {
 			//2. 연결하기
 			String url = "jdbc:mysql://localhost:3306/webdb";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
-			
+	
 			//3. Statement 객체 생성
-			stmt = conn.createStatement();
-			
+			stmt = conn.createStatement();			
+		
 			//4. SQL문 실행
 			String sql = 
-				"select no," +
-				"       first_name," +
-				"       last_name," + 
-				"       email" + 
-				"  from emaillist";
+				" insert" + 
+				"   into emaillist" +
+				" values (null, '"+ firstName + "', '" + lastName + "', '" + email + "')"; 
 			
-			rs = stmt.executeQuery(sql);
-			
-			//5. 결과 가져오기
-			while(rs.next()) {
-				Long no = rs.getLong(1);
-				String firstName = rs.getString(2);
-				String lastName = rs.getString(3);
-				String email = rs.getString(4);
-				
-				System.out.println(no + ":" + firstName + ":" + lastName + ":" + email);
-			}
+			int count = stmt.executeUpdate(sql);
+			result = (count == 1);
 			
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
@@ -50,13 +44,6 @@ public class SelectTest {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				//6.자원 정리
-				if(rs != null) {
-					rs.close();
-				}
-				if(stmt != null) {
-					stmt.close();
-				}
 				if(conn != null) {
 					conn.close();
 				}
@@ -64,5 +51,7 @@ public class SelectTest {
 				e.printStackTrace();
 			}
 		}
+		
+		return result;
 	}
 }
